7d9570795026767fa11c125c28124f691adc44d3
nadrojb <jordanbrewer1994@gmail.com>
2025-01-31 08:38:11 +0000

    created a new test for a successful api response

--- tests/Feature/CountriesControllerTest.php
+++ tests/Feature/CountriesControllerTest.php
@@ -2,31 +2,38 @@
 
 use App\Services\APIService;
 use GuzzleHttp\Psr7\Response;
-use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
 
-test('returns successful response for endpoint', function() {
+it('returns successful response for endpoint', function () {
     $response = $this->get('/api/v1/countries');
 
     $response->assertStatus(200);
 });
 
-test('returns 404 response for invalid endpoint', function () {
+it('returns 404 response for invalid endpoint', function () {
     $response = $this->get('/api/v1/countrie');
 
     $response->assertStatus(404);
 });
 
-it('successful api request', function () {
+it('makes a successful API request and returns the correct country data', function () {
+
+    $mockResponse = json_encode([
+        "name" => "Afghanistan",
+        "capital" => "Kabul",
+        "iso2" => "AF",
+        "iso3" => "AFG"
+    ]);
+
+
     $mockClient = Mockery::mock(Client::class);
     $mockClient->shouldReceive('get')
         ->with(config('services.countries_now.url'), ['timeout' => 30])
-        ->andReturn(new Response(200, [], json_encode(['country' => 'USA', 'capital' => 'Washington D.C.'])));
+        ->andReturn(new Response(200, [], $mockResponse));
+
 
     $apiService = new APIService($mockClient);
     $response = $apiService->makeApiRequest();
 
-    expect($response)->toBe(json_encode(['country' => 'USA', 'capital' => 'Washington D.C.']));
-
-
+    expect($response)->toBe($mockResponse);
 });
